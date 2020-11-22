57cad4b87063f089e8bd963972f8e28d5c74db6b
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2020-11-22 18:28:47 -0300

    create Appointment Model/Class

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,16 +1,10 @@
 import { Router } from 'express';
-import { v4 as uuid } from 'uuid';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
 
 const appointmentsRouter = Router();
 
-interface IAppointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
-
-const appointments: IAppointment[] = [];
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
@@ -26,11 +20,7 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked.' });
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
 
