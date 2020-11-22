f2fc65268fabb92ceab591dcdf7c5c5ff40c4ec3
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2020-11-22 18:15:44 -0300

    Initial commit + appointment

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,40 @@
+import { Router } from 'express';
+import { v4 as uuid } from 'uuid';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+const appointmentsRouter = Router();
+
+interface IAppointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: IAppointment[] = [];
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked.' });
+  }
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date: parsedDate,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
