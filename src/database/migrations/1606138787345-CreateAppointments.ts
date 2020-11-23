8389517f5ed7c5c6c0b56d6ef221c7e554ca1c8b
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2020-11-23 11:29:20 -0300

    Save data on Postgres (Persistence)

--- src/database/migrations/1606138787345-CreateAppointments.ts
+++ src/database/migrations/1606138787345-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1606138787345
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
